98e54ea92ae7d741d3a71a6e691be35055353054
HUSHAN MUBARIK KHAN <hushain1990@gmail.com>
2017-12-18 17:41:56 +0530

    Hushain @ Commit

--- SwiftLib/SwiftLib/Utilities/Color.swift
+++ SwiftLib/SwiftLib/Utilities/Color.swift
@@ -42,24 +42,13 @@ extension UIColor {
 			red: r/255.0,
 			green: g/255.0,
 			blue: b/255.0,
-			alpha: alpha/255.0
+			alpha: alpha
 		)
 	}
 	
 	//App Spacific Colors
-	static let cGradientColorOne 	= UIColor(r: 84.0, g: 64.0, b: 105.0)
-	static let cGradientColorTwo 	= UIColor(r: 47.0, g: 57.0, b: 99.0)
-	
-	static let cTabBackgroundColor 	= UIColor(r: 47.0, g: 57.0, b: 99.0)
-	static let cTabIconSelectedColor = UIColor(r: 251.0, g: 157.0, b: 55.0)
-	
-	static let cOrangeColor = UIColor(r: 245.0, g: 166.0, b: 35.0)  //f5a623
-	static let cGrayColor_TabDisable = UIColor(r: 255.0, g: 255.0, b: 255.0, alpha:0.5) //ffffff
 	static let cWhite_90			 = UIColor(r: 255.0, g: 255.0, b: 255.0, alpha:0.9) //ffffff
 	static let cGray_Background      = UIColor(r: 231.0, g: 231.0, b: 231.0) //e7e7e7
-	//static let
 	
-	static let cTransparentColor_White = UIColor(r: 255.0, g: 255.0, b: 255.0, alpha:0.1)
-	static let cTransparentColor_Black = UIColor(r: 0.0, g: 0.0, b: 0.0, alpha:0.1)
 	
 }
